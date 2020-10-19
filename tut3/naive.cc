dd698ebd1dd19efcf121ce6445efab66243a8ecd
Radomír Polách <rp@t4d.cz>
2020-10-19 23:27:53 +0200

    feature: improve naive matching

--- tut3/naive.cc
+++ tut3/naive.cc
@@ -11,6 +11,30 @@
  * Author: Jakub Kulik
  */
 
+inline void naive(char* string, char* pattern, int size, int psize) {
+  int i, j;
+
+  printf("%s\n", string);
+  for (i = 0; i < size; i++) {
+    //printf("Comparing string[%d] with pattern[%d].\n", i, j);
+    for (j = 0; j < psize; j++) {
+      if (string[i+j] == pattern[j]) {
+        printf("%c", pattern[j]);
+        if (j+1 == psize) {
+	  printf("%*c  match on position %d", size-i, ' ', i);
+          printf("\n%*c", i+1, ' ');
+          break;
+        }
+      } else {
+        printf("%c\n", pattern[j]);
+        printf("%*c", i+1, ' ');
+        break;
+      }
+    }
+  }
+  printf("\n");
+}
+
 int main(int argc, char **argv) {
   int i, size, psize;
 
@@ -19,9 +43,7 @@ int main(int argc, char **argv) {
   size = strlen(argv[1]);
   psize = strlen(argv[2]);
 
-  for (i = 0; i <= size - psize; i++)
-    if (! strncmp(argv[1] + i, argv[2], psize))
-      printf("%d\n", i);
+  naive(argv[1], argv[2], size, psize);
 
   return EXIT_SUCCESS;
 }
